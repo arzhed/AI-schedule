3f8015aaf444f8ce1dcdbb1bb83414306d65f259
Charles Cote <charlescote9@gmail.com>
2013-11-22 11:51:44 -0700

    Added Solution class

---
+++ Solution.java
@@ -0,0 +1,18 @@
+package taAllocation;
+
+import java.util.Vector;
+
+public class Solution {
+	
+	private Vector<Pair<Lab, TA>> solution = new Vector<Pair<Lab, TA>>;
+	
+	public void addElement (Pair<Lab, TA> elem) {
+		solution.add(elem);
+	}
+	
+	public Vector<Pair<Lab, TA>> getSolution() {
+		return solution;
+	}
+	
+}
+		
